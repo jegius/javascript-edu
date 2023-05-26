8b46e59ad31975a77cb9b2b91a99cb30933eaef5
Ivan Alekseev <iv.s.alekseev@tinkoff.ru>
2023-05-26 18:35:41 +0300

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,39 @@
+const messageService = (function () {
+    const messageStore = ['test message', 'test message', 'test message'];
+    const callbackStore = [];
+
+    return {
+        send: (message) => {
+            messageStore.push(message);
+            callbackStore.map(callback => callback(messageStore))
+
+        },
+        subscribe: (callback) => {
+            callbackStore.push(callback);
+            callback(messageStore);
+        }
+    }
+})()
+
+
+function paintMessages(messages) {
+    const messageContainer = document.querySelector('.messages');
+    messageContainer.innerHTML = null;
+
+    messages.map(message => {
+        const messageBubble = document.createElement('div');
+        messageBubble.innerHTML = message;
+        messageContainer.append(messageBubble)
+    })
+}
+
+messageService.subscribe(paintMessages);
+
+const addButton = document.querySelector('.addButton');
+const messageBody = document.querySelector('.message-body');
+
+addButton.addEventListener('click', (event) => {
+    event.stopImmediatePropagation();
+    messageService.send(messageBody.value)
+    messageBody.value = null;
+})
